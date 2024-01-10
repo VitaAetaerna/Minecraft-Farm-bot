7dcde0af4345075554982b736ef9a2849996bca8
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-10 20:21:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,47 +6,51 @@ running = False
 
 try:
     from pynput.mouse import Button, Controller as MouseController
+    from win32gui import GetWindowText, GetForegroundWindow
     import keyboard
 except ImportError:
     os.system('cmd /k pip install pynput')
     os.system('cmd /k pip install keyboard')
+    os.system('cmd /k pip install pywin32')
 
-
+def GetNewTime():
+    endTime = datetime.datetime.now() + datetime.timedelta(minutes=1.42)
+    return endTime
 
 def Bot():
+    running = False
     mouse = MouseController()
     keyboard.wait(',')
-    print('Starting...')
+    print('Starting in 5 seconds...')
     running = True
     time.sleep(5)
+    
 
     while running:
-        endTime = datetime.datetime.now() + datetime.timedelta(minutes=1.42)
-        while True:
-            time.sleep(0.2)
+        while datetime.datetime.now() < GetNewTime() and GetWindowText(GetForegroundWindow()) == "Minecraft* 1.20 - Multiplayer (3rd-party Server)":
+            time.sleep(0.1)
             keyboard.press('w')
-            time.sleep(0.2)
+            time.sleep(0.1)
             keyboard.press('a')
             mouse.click(Button.left, 1)
-            if datetime.datetime.now() >= endTime:
+            if datetime.datetime.now() >= GetNewTime():
                 break
 
 
-        endTime = datetime.datetime.now() + datetime.timedelta(minutes=1.42)
-        while True:
-            time.sleep(0.2)
+        while datetime.datetime.now() < GetNewTime() and GetWindowText(GetForegroundWindow()) == "Minecraft* 1.20 - Multiplayer (3rd-party Server)":
+            time.sleep(0.1)
             keyboard.press('w')
-            time.sleep(0.2)
+            time.sleep(0.1)
             keyboard.press('d')
             mouse.click(Button.left, 1)
-            if datetime.datetime.now() >= endTime:
+            if datetime.datetime.now() >= GetNewTime():
                 break
 
         event = keyboard.read_event()
         if event.event_type == keyboard.KEY_DOWN and event.name == 'l':
             keyboard.wait(',')
-            running = False
             Bot()
+            
 
 if __name__ == "__main__":
     Bot()
